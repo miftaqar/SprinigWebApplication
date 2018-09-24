43fa12ba5fdcdc0f53dfa38995dedf528794da90
mifteqar <ifteqar.ahmed01@gmail.com>
2018-09-24 15:45:55 +0530

    Practiced SpringMVC from gontuSeries, code included part 5 & 6

--- src/main/java/com/primetgi/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/primetgi/helloworld/controller/HelloWorldController.java
@@ -1,22 +1,19 @@
 package com.primetgi.helloworld.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
 
-@Controller
-public class HelloWorldController {
-	String message = "Welcome to Spring MVC";
+//Each controller needs to extend BaseController class
+public class HelloWorldController extends AbstractController {
 
-	@RequestMapping("/hello")
-	public ModelAndView showMessage(
-			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
-		System.out.println("In Controller");
-
-		ModelAndView modelViewObj = new ModelAndView("helloworld");
-		modelViewObj.addObject("message", message);
-		modelViewObj.addObject("name", name);
-		return modelViewObj;
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		ModelAndView modelAndView = new ModelAndView("helloworld");
+		modelAndView.addObject("msg", "Welcome to first Spring Application");
+		return modelAndView;
 	}
 }
--- src/main/webapp/WEB-INF/views/helloworld.jsp
+++ src/main/webapp/WEB-INF/views/helloworld.jsp
@@ -8,6 +8,6 @@
 </head>
 <body>
 	<h2>Hello World</h2>
-	<h2>${message} ${name}</h2>
+	<h2>${msg}</h2>
 </body>
 </html>
