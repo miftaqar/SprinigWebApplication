04f24cc1af16f63fc4da1025ba0e0670082cc9f5
mifteqar <ifteqar.ahmed01@gmail.com>
2018-09-25 16:52:11 +0530

    Practiced SpringMVC from gontuSeries, code included part 11-1(handling form data - Added @ModelAttribute annotation, and using studentModel Object to access form data in jsp page)

--- src/main/java/com/primetgi/studentadmission/controller/StudentAdmissionController.java
+++ src/main/java/com/primetgi/studentadmission/controller/StudentAdmissionController.java
@@ -3,6 +3,7 @@ package com.primetgi.studentadmission.controller;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -44,20 +45,31 @@ public class StudentAdmissionController {
 	// return modelAndView;
 	// }
 
+	// @RequestMapping(value = "/submitAdmissionForm.html", method =
+	// RequestMethod.POST)
+	// public ModelAndView submitAdmissionForm(
+	// @RequestParam(value = "studentName", defaultValue = "Mohd Ifteqar Ahmed")
+	// String name,
+	// @RequestParam("studentHobby") String hobby) {
+	//
+	// Student student = new Student();
+	// student.setStudentName(name);
+	// student.setStudentHobby(hobby);
+	//
+	// ModelAndView modelAndView = new ModelAndView("admissionSuccess");
+	// modelAndView.addObject("message", "Here are your details, Name: "
+	// + name + " and your Hobby: " + hobby);
+	// modelAndView.addObject("student", student);
+	//
+	// return modelAndView;
+	// }
+
 	@RequestMapping(value = "/submitAdmissionForm.html", method = RequestMethod.POST)
 	public ModelAndView submitAdmissionForm(
-			@RequestParam(value = "studentName", defaultValue = "Mohd Ifteqar Ahmed") String name,
-			@RequestParam("studentHobby") String hobby) {
-
-		Student student = new Student();
-		student.setStudentName(name);
-		student.setStudentHobby(hobby);
+			@ModelAttribute("student") Student student) {
 
 		ModelAndView modelAndView = new ModelAndView("admissionSuccess");
-		modelAndView.addObject("message", "Here are your details, Name: "
-				+ name + " and your Hobby: " + hobby);
-		modelAndView.addObject("student", student);
-		
+
 		return modelAndView;
 	}
 
